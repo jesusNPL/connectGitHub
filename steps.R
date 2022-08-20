ad75d3de316ee0148318da1c1dd9c0f27347f5e0
Jesús N. Pinto-Ledezma <jesuspintoledezma@gmail.com>
2022-08-20 16:31:25 -0500

    update

--- steps.R
+++ steps.R
@@ -1,10 +1,12 @@
 # step 1 - connect with github
 usethis::use_git()
-# step 2 - 
+# step 2 - try using github
 usethis::use_github()
-# step 3 - generate github token to access
+# step 3 - generate token to get access to github
 usethis::create_github_token()
-# step 4 - set credits in rstudio to get access to github
+# step 4 - set credential in rstudio to get access to github
 gitcreds::gitcreds_set()
-# step 5 - use github 
+# step 5 - try using github again
 usethis::use_github()
+
+## success!
